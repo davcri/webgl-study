3d8d09c8f9442c733e374b9d26430fa13d96cea1
Davide Cristini <davcri91@gmail.com>
2020-09-10 19:47:32 +0200

    Add 10-z-fighting.
    I'm not sure this example is working as intended

---
+++ src/ch07-toward-the-3d-world/10-z-fighting/index.js
@@ -0,0 +1,139 @@
+import WebGLApp from '../../libs/mgu/webgl-app.js';
+import Matrix4 from '../../libs/cuon-matrix.js';
+import Vector4 from '../../libs/mgu/vector4.js';
+
+class App extends WebGLApp {
+  ready() {
+    this.registerOnKeyDown();
+    this.addNotes();
+
+    this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.enable(this.gl.POLYGON_OFFSET_FILL);
+    this.u_MvpMatrix = this.getUniformLocation('u_MvpMatrix');
+    const vertexBuffer = this.createBuffer();
+
+    this.perspectiveMode = true;
+
+    const verticesData = App.initVertices();
+    this.n = 6; // vertices
+    this.nearPlane = -5.0;
+    this.farPlane = 5.0;
+
+    this.projMatrix = new Matrix4();
+    this.viewMatrix = new Matrix4();
+    this.modelMatrix = new Matrix4().setIdentity().setTranslate(0.5, 0, 0);
+
+    this.eye = new Vector4(0, 0, 20);
+    this.center = new Vector4(0, 0, -100); // look at point
+    this.up = new Vector4(0, 1, 0);
+    this.viewMatrix.setLookAt(
+      this.eye.x, this.eye.y, this.eye.z,
+      this.center.x, this.center.y, this.center.z,
+      this.up.x, this.up.y, this.up.z,
+    );
+
+    // see "stride" and "offset" here: https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/vertexAttribPointer
+    const stride = 6 * verticesData.BYTES_PER_ELEMENT;
+    const positionOffset = 0;
+    const colorOffset = 3 * verticesData.BYTES_PER_ELEMENT;
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vertexBuffer);
+
+    const a_Position = this.getAttribLocation('a_Position');
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, verticesData, this.gl.STATIC_DRAW);
+    this.gl.vertexAttribPointer(a_Position, 3, this.gl.FLOAT, false, stride, positionOffset);
+    this.gl.enableVertexAttribArray(a_Position);
+
+    const a_Color = this.getAttribLocation('a_Color');
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, verticesData, this.gl.STATIC_DRAW);
+    this.gl.vertexAttribPointer(a_Color, 3, this.gl.FLOAT, false, stride, colorOffset);
+    this.gl.enableVertexAttribArray(a_Color);
+
+    this.gl.clearColor(0.1, 0.1, 0.1, 1); // set black as clear color
+    this.draw();
+  }
+
+  static initVertices() {
+    const triangle1 = [
+      // x y z , r g b
+      0.0, 2.5, -5, 0.0, 1.0, 1.0,
+      -2.5, -2.5, -5, 0.0, 1.0, 0.0,
+      2.5, -2.5, -5, 0.0, 1.0, 0.0,
+    ];
+
+    const triangle2 = [
+      0.0, 3.0, -5, 1.0, 0.0, 0.0,
+      -3.0, -3.0, -5, 1.0, 1.0, 0.0,
+      3.0, -3.0, -5, 1.0, 1.0, 0.0,
+    ];
+    return new Float32Array([
+      // vec3, vec3 rgb
+      ...triangle2,
+      ...triangle1,
+    ]);
+  }
+
+  registerOnKeyDown() {
+    document.onkeydown = (ev) => {
+      this.onKeyDown(ev);
+    };
+  }
+
+  onKeyDown(ev) {
+    const KEY_LEFT = 37;
+    const KEY_RIGHT = 39;
+    // this.perspectiveMode = !this.perspectiveMode;
+    if (ev.keyCode === KEY_LEFT) {
+      // this.modelMatrix.translate(-0.1, 0, 0);
+      this.modelMatrix.rotate(5, 0, 1, 0);
+    }
+    if (ev.keyCode === KEY_RIGHT) {
+      this.modelMatrix.rotate(-5, 0, 1, 0);
+      // this.modelMatrix.translate(0.1, 0, 0);
+    }
+    this.draw();
+  }
+
+  process() {
+    // this.draw();
+  }
+
+  draw() {
+    this.viewMatrix.setLookAt(
+      this.eye.x, this.eye.y, this.eye.z,
+      this.center.x, this.center.y, this.center.z,
+      this.up.x, this.up.y, this.up.z,
+    );
+
+    if (this.perspectiveMode) {
+      this.projMatrix.setPerspective(
+        30, // fov y
+        this.canvas.width / this.canvas.height, // aspect ratio of the frustum
+        1, // near plane
+        100, // far plane
+      );
+    } else {
+      this.projMatrix = this.projMatrix.setOrtho(-1, 1, -1, 1, -10, 22);
+    }
+
+    this.mvpMatrix = this.projMatrix.multiply(this.viewMatrix).multiply(this.modelMatrix);
+    this.gl.uniformMatrix4fv(this.u_MvpMatrix, false, this.mvpMatrix.elements);
+
+    // draw
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, this.n / 2);
+    this.gl.polygonOffset(1.0, 0.0);
+    this.gl.drawArrays(this.gl.TRIANGLES, this.n / 2, this.n / 2);
+  }
+
+  addNotes() {
+    const el = document.getElementsByClassName('notes').item(0);
+    el.innerHTML = `WARNING: I'm not sure this example
+    is working as intended.<br>
+    If you rotate the triangles with keys, z-fighting is still occurring.`;
+  }
+}
+
+
+const app = new App();
+app.start();
