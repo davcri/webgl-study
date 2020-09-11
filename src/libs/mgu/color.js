1d2f978f679a665323393cfdde648a4874cb5c0b
Davide Cristini <davcri91@gmail.com>
2020-09-11 18:14:37 +0200

    Add COLORS constant to color.js

--- src/libs/mgu/color.js
+++ src/libs/mgu/color.js
@@ -15,3 +15,14 @@ export default class Color {
     );
   }
 }
+
+export const COLORS = {
+  white: [1, 1, 1],
+  magenta: [1, 0, 1],
+  red: [1, 0, 0],
+  black: [0, 0, 0],
+  yellow: [1, 1, 0],
+  green: [0, 1, 0],
+  cyan: [0, 1, 1],
+  blue: [0, 0, 1],
+};
